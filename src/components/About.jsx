0c692ce41aa57fa4f17c27e4e202e8ce3f92b460
ktxdev <sean.ktxdev@gmail.com>
2022-03-13 16:48:08 +0200

    Change text size for name

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,8 +5,8 @@ const About = () => {
     return (
         <div className='w-full flex flex-col-reverse items-center py-16 px-5 text-center md:flex-row md:justify-between md:text-left md:py-40 md:px-40'>
             <div className="font-montserrat mt-16">
-                <h4 className="font-roboto font-bold text-red-500" >Hi, my name is</h4>
-                <h2 className="text-2xl">Sean Huvaya</h2>
+                <h4 className="font-roboto font-bold text-red-500 text-lg" >Hi, my name is</h4>
+                <h2 className="text-3xl font-bold">Sean Huvaya</h2>
                 <p className="mt-6 mb-12 leading-8 tracking-widest max-w-md md:max-w-xl">
                 I'm a <strong>Software developer</strong> with 2+ years of progressive experience in software development. Involved in the design, development, testing, and maintenance of enterprise-level web applications and spring-based microservices using Java/Java EE technologies.
                 </p>
