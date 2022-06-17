fd001655fdd1d04176d63e1ad36ca1f1166bf5fe
ktxdev <sean.ktxdev@gmail.com>
2022-06-18 00:22:50 +0200

    Change styles

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -5,7 +5,7 @@ const About = () => {
     return (
         <div id='summary' className='w-full flex flex-col-reverse items-center px-5 md:px-10 lg:px-44 xl:px-60 pt-40 pb-20 md:py-48 text-center md:flex-row md:justify-center md:gap-28 md:text-left'>
             <div className="font-montserrat mt-16">
-                <h4 className="font-roboto font-bold text-red-500 sm:text-base md:text-lg" >Hi, my name is</h4>
+                <h4 className="font-roboto font-bold text-red-500 font-light" >Hi, my name is</h4>
                 <h2 className="text-3xl xl:text-5xl font-bold my-1">Sean Huvaya.</h2>
                 <h2 className="text-2xl xl:text-4xl mt-3 tracking-wide text-gray-600 font-bold my-1">I'm a Backend Software Developer.</h2>
                 <p className="mt-6 mb-12 leading-8 tracking-widest max-w-md lg:max-w-xl">
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -8,7 +8,7 @@ const Skills = () => {
 
     return (
         <div className='pt-20 pb-10'>
-            <h6 className='font-roboto font-bold text-red-500 text-center sm:text-xs md:text-lg'>Here are the technologies I've been working with recently</h6>
+            <h6 className='font-roboto text-red-500 text-center font-light '>Here are the technologies I've been working with recently</h6>
             <div className='grid grid-cols-2 md:grid-cols-3 gap-8 mt-10' >
                 <div>
                     <h4 className='font-bold font-roboto my-2'>Language</h4>
