b4bab80852be830fafad6842584d89238ec6726d
ktxdev <sean.ktxdev@gmail.com>
2022-06-19 20:27:08 +0200

    Created an image carousel

--- src/data/projects.js
+++ src/data/projects.js
@@ -4,14 +4,18 @@ const projects = [
         description: 'Bug tracker is a simple crud application that can be used to track bugs, issues and new features for your projects.',
         technologies: ['Java 11', 'Spring Boot', 'JavaScript', 'ReactJS', 'HTML 5', 'CSS3', 'Heroku' ],
         screenshots: [
-            'https://github.com/ktxdev/expense-tracker/blob/main/docs/demo-1.png?raw=true',
-            'https://github.com/ktxdev/expense-tracker/blob/main/docs/demo-2.png?raw=true',
-            'https://github.com/ktxdev/expense-tracker/blob/main/docs/demo-3.png?raw=true',
-            'https://github.com/ktxdev/expense-tracker/blob/main/docs/demo-4.png?raw=true',
-            'https://github.com/ktxdev/expense-tracker/blob/main/docs/demo-5.png?raw=true'
+            'https://github.com/ktxdev/bug-tracker/blob/master/docs/screenshots/screenshot-1.png?raw=true',
+            'https://github.com/ktxdev/bug-tracker/blob/master/docs/screenshots/screenshot-2.png?raw=true',
+            'https://github.com/ktxdev/bug-tracker/blob/master/docs/screenshots/screenshot-3.png?raw=true',
+            'https://github.com/ktxdev/bug-tracker/blob/master/docs/screenshots/screenshot-4.png?raw=true',
+            'https://github.com/ktxdev/bug-tracker/blob/master/docs/screenshots/screenshot-5.png?raw=true',
+            'https://github.com/ktxdev/bug-tracker/blob/master/docs/screenshots/screenshot-6.png?raw=true',
+            'https://github.com/ktxdev/bug-tracker/blob/master/docs/screenshots/screenshot-7.png?raw=true',
+            'https://github.com/ktxdev/bug-tracker/blob/master/docs/screenshots/screenshot-8.png?raw=true',
+            'https://github.com/ktxdev/bug-tracker/blob/master/docs/screenshots/screenshot-9.png?raw=true'
         ],
-        githubLink: 'https://github.com/ktxdev/expense-tracker',
-        liveLink: 'https://ktxdev-et.herokuapp.com'
+        githubLink: 'https://github.com/ktxdev/bug-tracker',
+        liveLink: 'https://ktxdev-bt.herokuapp.com'
     },
     {
         title: 'Expense Tracker',
